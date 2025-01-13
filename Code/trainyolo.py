ed4ef1239e46f8ab6e6c1f254d535624202e56d6
Arshan Abbas <arshan.abbas@student.uni-siegen.de>
2025-01-13 14:52:03 +0100

    changes

--- Code/trainyolo.py
+++ Code/trainyolo.py
@@ -8,4 +8,4 @@ model = YOLO("yolo11n-seg.pt")  # Ensure the model file is correctly downloaded 
 data = 'C:/Users/arab/Documents/GitHub/Thesis-Abbas/Code/dataset.yaml'
 
 # Start training
-results = model.train(data=data, epochs=100, imgsz=640)  # Ensure the arguments match the expected API
+results = model.train(data=data, epochs=1, imgsz=1024)  # Ensure the arguments match the expected API
