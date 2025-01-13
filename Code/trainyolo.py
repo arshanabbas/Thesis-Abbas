2ecd69b76f260ce5d5c9c445c633add7bd43e095
Arshan Abbas <arshan.abbas@student.uni-siegen.de>
2025-01-13 15:03:25 +0100

    changes

--- Code/trainyolo.py
+++ Code/trainyolo.py
@@ -2,10 +2,10 @@ import os
 from ultralytics import YOLO
 
 # Load the pre-trained model
-model = YOLO("yolo11n-seg.pt")  # Ensure the model file is correctly downloaded and accessible
+model = YOLO("D:/Abbas/GitHub/PolygontoYOLO/yolo11n-seg.pt")
 
 # Path to the dataset configuration file
 data = 'C:/Users/arab/Documents/GitHub/Thesis-Abbas/Code/dataset.yaml'
 
 # Start training
-results = model.train(data=data, epochs=1, imgsz=1024)  # Ensure the arguments match the expected API
+results = model.train(data= "D:/Abbas/GitHub/Thesis-Abbas/Code/dataset.yaml", epochs=1, imgsz=1024)
