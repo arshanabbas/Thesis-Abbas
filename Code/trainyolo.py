51ea629aaa1e0c94bbb8fb9e62185f96254cf567
Arshan Abbas <arshan.abbas@student.uni-siegen.de>
2025-04-08 14:52:09 +0200

    changes

--- Code/trainyolo.py
+++ Code/trainyolo.py
@@ -1,12 +1,20 @@
 from ultralytics import YOLO
 
-# Load a pretrained YOLO11n model
-model = YOLO("yolo11n.pt")
+def main():
+    model = YOLO("yolo11n.pt")
 
-# Train the model on the COCO8 dataset for 100 epochs
-train_results = model.train(
-    data="train.yaml",  # Path to dataset configuration file
-    epochs=300,  # Number of training epochs
-    imgsz=640,  # Image size for training
-    device=0,  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
-)
+    train_results = model.train(
+        data="train.yaml",  # Path to the dataset configuration file
+        epochs=300,  # Set the number of training epochs
+        imgsz=640,  # Image size for training (input size for model)
+        device=0,  # Specify the device (0 for the first GPU, 'cpu' for CPU)
+    )
+
+    # After training, evaluate the model's performance on the validation set
+    metrics = model.val()
+
+    # Optionally, print or save evaluation metrics
+    print(metrics)
+
+if __name__ == '__main__':
+    main()
